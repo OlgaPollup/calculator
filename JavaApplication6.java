210787d42ba88295f26469be0b982e2823407fdc
OlgaPollup <olga.pollup@outlook.com>
2016-09-09 10:36:04 +0300

    калькулятор выполняет все действия

---
+++ JavaApplication6.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer number1, number2, sum;
+        String znak;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        number1=scanner.nextInt();
+        System.out.println("Введите второе число");
+        number2=scanner.nextInt();
+        
+        System.out.println("Введите действие (+ или - или * или /)");
+        
+        znak = scanner.next();
+        sum = 0;
+        switch (znak) {
+            case "+":
+                sum=number1+number2;
+                break;
+             case "-":
+                sum=number1-number2;
+                break;
+             case "*":
+                sum=number1*number2;
+                break;  
+            case "/":
+                if (number2 !=0){
+                    sum=number1/number2;
+                }
+                else {
+                System.out.println("Деление на ноль невозможно");}
+                break;
+            default:
+                System.out.println("Неправильно введены числа");
+                sum=null;
+        }
+        if(sum !=null)
+        System.out.println("Результат" +sum );
+    }
+    
+}
